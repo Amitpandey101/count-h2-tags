59ad4f8bcbbe3ea9d47e5cd24dc1eeb4f88d1bdf
Amit pandey <102383593+Amitpandey101@users.noreply.github.com>
2022-05-23 06:36:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let headings=document.getElementsByTagName("h2");
+let button=document.getElementById("btn");
+button.addEventListener("click",()=>{
+window.alert(`total h2 tags are: ${headings.length}`)
+
+  
+})
